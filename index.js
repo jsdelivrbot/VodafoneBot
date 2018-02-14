6a4c72b8a3d745f58f5965f5058fb24c46610cc6
marcodagolini <marco.dagolini@gmail.com>
2018-02-15 00:52:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var https = require('https');
 var express = require('express');
 const Agent = require('./node-agent-sdk/AgentSDK');
-var choAgent = new Agent({
+var echoAgent = new Agent({
   accountId: '13099967',
   username: 'botLivePerson',
   password: 'Password123!!!'
